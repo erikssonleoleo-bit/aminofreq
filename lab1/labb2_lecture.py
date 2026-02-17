e62937234b4019ccd721c5a47eb2d6dc4eb32d10
erikssonleoleo-bit <erikssonleoleo@gmail.com>
2026-02-17 12:28:08 +0100

    föreläsning måndag 2

--- lab1/labb2_lecture.py
+++ lab1/labb2_lecture.py
@@ -1,1 +1,5 @@
 #testmodulen är samma som labb1, vi fyller i filerna model och view, 
+# tredje frågan på tentan kommer vara att man ska skapa en class
+#class Particle
+    #def __init__ (self, ....):
+        #   self.mass
