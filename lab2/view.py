b854cd4b2169e05019fe5abd5fa95c68fe38f183
oskarjohansson2004 <oskarjohansson2004@gmail.com>
2026-02-21 16:16:54 +0100

    View

--- lab2/view.py
+++ lab2/view.py
@@ -1,6 +1,24 @@
-# Task (7/12): Draw on canvas
+# Task 7/12
+
+from tkinter import *
+from model import *
+root = Tk()
+canvas = Canvas(root, bg="white", width=800, height=600)
+canvas.pack()
+o = canvas.create_oval(80, 30, 140, 150, fill="blue")
+input()
+
+
 
 # Task (8/12): Define a new function to_canvas_coords(canvas, x)
+
+def to_canvas_coords(canvas, x):
+    Vec.x = x
+    h = canvas.winfo_reqheight()
+    scale = h / 20
+    Vec.x = Vec.x * scale
+
+
 
 #######################################
 ### NB. Task 9 is done in model.py. ###
@@ -10,4 +28,4 @@
 
 # Task (11/12): Define a new function create_oval(canvas, particle)
 
-# Task (12/12): Define a function simulation_loop(f, timestep, particles)
+# Task (12/12): Define a function simulation_loop(f, timestep, particles
