39e7f0623d82c47e75143fcfabdff322f2f1297c
erikssonleoleo-bit <erikssonleoleo@gmail.com>
2026-03-05 08:17:40 +0100

    sim lopp

--- lab2/view.py
+++ lab2/view.py
@@ -30,7 +30,9 @@ def move_oval(o, u1, u2):
 # Task (11/12): Define a new function create_oval(canvas, particle)
 def create_oval(canvas, particle):
     o = canvas.create_oval(0,0,0,0, fill = "blue")
-    o = move_oval(o, particle)
+    u1, u2 = particle.bounding_box()
+     # Pass u1 and u2 to move_oval (and remember move_oval returns None, so don't assign it to 'o')
+    move_oval(o, u1, u2)
     return o
 
 
@@ -42,3 +44,31 @@ for n in range(5):
     time.sleep(1)
 
 # Task (12/12): Define a function simulation_loop(f, timestep, particles)
+def simulation_loop(f, timestep, particles):
+    # Set up one graphical object (oval) for each particle
+    ovals = []
+    for p in particles:
+        ovals.append(create_oval(canvas, p))
+        
+    # Repeat the following steps in an infinite loop
+    while True:
+        # 1. call f(timestep, particles)
+        f(timestep, particles)
+        
+        # 2 & 3. Move the particle and move the oval
+        for i in range(len(particles)):
+            p = particles[i]
+            o = ovals[i]
+            
+            # 2. move the particle according to the inertial Newton law
+            p.inertial_move(timestep)
+            
+            # 3. move each oval to the new position of the particle
+            u1, u2 = p.bounding_box()
+            move_oval(o, u1, u2)
+            
+        # At the end of step 3, update the canvas
+        canvas.update()
+        
+        # (Optional) Add a slight delay so the animation runs smoothly 
+        # time.sleep(timestep)
