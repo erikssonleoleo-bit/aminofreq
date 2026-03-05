98e4de3aec2006f4c050394a18ae3a72f7b9d4e8
erikssonleoleo-bit <erikssonleoleo@gmail.com>
2026-03-05 09:14:32 +0100

    walls

--- lab2/experiment_walls.py
+++ lab2/experiment_walls.py
@@ -13,7 +13,23 @@ for i in range(n)
 
 
 def combined_walls(dt, particles):
-    k = 5 
-    a_
+    # Fjäderkonstanten k (hur "hårda" väggarna är) enligt instruktionerna
+    k = 5  
+    
+    # 1. Golv (Bottenvägg)
+    # Ankarpunkt på y = -10. Normalvektorn pekar UPPÅT (0, 1) in mot mitten.
+    wall_force(dt, particles, k, Vec(0, 1), Vec(0, -10))
+    
+    # 2. Tak (Toppvägg)
+    # Ankarpunkt på y = 10. Normalvektorn pekar NEDÅT (0, -1) in mot mitten.
+    wall_force(dt, particles, k, Vec(0, -1), Vec(0, 10))
+    
+    # 3. Vänster vägg
+    # Ankarpunkt på x = -10. Normalvektorn pekar åt HÖGER (1, 0) in mot mitten.
+    wall_force(dt, particles, k, Vec(1, 0), Vec(-10, 0))
+    
+    # 4. Höger vägg
+    # Ankarpunkt på x = 10. Normalvektorn pekar åt VÄNSTER (-1, 0) in mot mitten.
+    wall_force(dt, particles, k, Vec(-1, 0), Vec(10, 0))
 
 simulation_loop(combined_walls, )
